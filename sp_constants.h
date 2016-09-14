8da0e0ad6d49e522c096321b5607df3ca1228274
Matan Eilat <mtneilat@gmail.com>
2016-09-14 22:11:17 +0300

    Added documentation to sp_constants.

--- sp_constants.h
+++ sp_constants.h
@@ -11,11 +11,28 @@
 #include <stdlib.h>
 #include "SPPoint.h"
 
+/**
+ * General constants to be used throughout.
+ */
+
+/**
+ * Feature extract function declaration.
+ * 	- First parameter is the image path.
+ * 	- Second parameter is the image index.
+ * 	- Third parameter is place-holder for the number of features extracted.
+ */
 typedef SPPoint *(*FeatureExractionFunction)(const char *, int, int *);
 
+/** The default configuration file name.*/
 #define DEFAULT_CONFIG_FILENAME "spcbir.config"
+
+/** Maximum size for a single line to be used in different scenarios. */
 #define LINE_MAX_SIZE 1024
+
+/** Error message for allocation failure. */
 #define ALLOCATION_ERROR_MSG "Memory allocation failed"
+
+/** Maximum size for a path to be used in different scenarios. */
 static const int MAX_PATH_LENGTH = 100;
 
 #endif /* SP_CONSTANTS_H_ */
